281970f903cbbd9db9a72bf84d763939a0e03984
Archit <architojha420@gmail.com>
2024-03-21 19:37:07 +0530

    integrated camera

--- app/src/main/java/com/example/opencv_project/MainActivity.java
+++ app/src/main/java/com/example/opencv_project/MainActivity.java
@@ -2,23 +2,69 @@ package com.example.opencv_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
-
+import android.Manifest;
+import org.opencv.android.CameraActivity;
+import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Mat;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Collections;
+import java.util.List;
+
+public class MainActivity extends CameraActivity {
+
+    CameraBridgeViewBase cameraBridgeViewBase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        camera_permission();
+
+        cameraBridgeViewBase=findViewById(R.id.cameraView);
+
+        cameraBridgeViewBase.setCameraIndex(0);
+
+        cameraBridgeViewBase.setCvCameraViewListener(new CameraBridgeViewBase.CvCameraViewListener2() {
+            @Override
+            public void onCameraViewStarted(int width, int height) {}
+
+            @Override
+            public void onCameraViewStopped() {
+
+            }
+
+            @Override
+            public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+                return inputFrame.rgba();
+            }
+        });
+
         if(OpenCVLoader.initDebug()){
-            Log.d("success","yessir");
+            cameraBridgeViewBase.enableView();
         }
-        else{
-            Log.d("failed","negga");
+    }
+    @Override
+    protected List<? extends CameraBridgeViewBase> getCameraViewList() {
+        return Collections.singletonList(cameraBridgeViewBase);
+    }
+
+    void camera_permission() {
+        if(checkSelfPermission(Manifest.permission.CAMERA)!= PackageManager.PERMISSION_GRANTED){
+            requestPermissions(new String[]{Manifest.permission.CAMERA}, 101);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if(grantResults.length>0 && grantResults[0]!=PackageManager.PERMISSION_GRANTED){
+            camera_permission();
         }
     }
 }
